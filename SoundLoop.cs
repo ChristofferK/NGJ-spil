a8e5f5bd4e2b8f3ec371fb7ec37a283022142945
Rewax <daniel.lorenzen@hotmail.com>
2016-04-06 11:13:46 +0200

    Added files via upload
    Sound loop when mouse0 is held down

---
+++ SoundLoop.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//husk at adde audio scource til game object
+
+
+public class SoundLoop : MonoBehaviour {
+
+
+	public AudioSource mysound;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			mysound.enabled = true;
+			mysound.loop = true;
+		} else {
+
+			mysound.enabled = false;
+			mysound.loop = false;
+
+		}
+	}
+}
