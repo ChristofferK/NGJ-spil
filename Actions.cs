c1d6635f0f7ed2f7dc8f063b68a847d9279d560c
Dazasaur <mikkel0_0@live.dk>
2016-04-09 19:43:56 +0200

    Detection & Actions
    Detection.cs => tileObject Script
    Actions.cs => gameObject Script

---
+++ Actions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actions : MonoBehaviour {
+
+    public int health = 100;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }	
+	}
+}
---
+++ detection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class detection : MonoBehaviour {
+
+    public bool doneOnce = false;
+    public GameObject Obj;
+    public GameObject playerObj;
+
+        // These two are linked in proportionality, so healthChange should always be 100*sizeChange 
+    public int healthChange = 10; // Change Health Change per tile 
+    public float sizeChange = 0.1f; // Change Scale X Y of Player Object
+    public float playerScale;
+	// Use this for initialization
+
+  
+    
+
+
+	void Start ()
+    {
+        playerObj = GameObject.Find("WhiteBox"); // WhiteBox = Name of Player Object
+        
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (!doneOnce)
+        {
+            playerScale = playerObj.transform.localScale.x;
+            playerScale -= sizeChange;
+            playerObj.GetComponent<Actions>().health -= healthChange;
+            playerObj.transform.localScale = new Vector2(playerScale, playerScale);
+            doneOnce = true;
+            Instantiate(Obj, transform.position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
